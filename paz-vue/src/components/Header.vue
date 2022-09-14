3641aaf0953ccda6a075ee4d83e6b6692b3a0731
Y12Y12 <youssef.khalil@hva.nl>
2022-09-14 20:18:00 +0200

    3.1) Header en welcome text

--- paz-vue/src/components/Header.vue
+++ paz-vue/src/components/Header.vue
@@ -1,13 +1,33 @@
 <template>
-<h1>Does "Header "work"?</h1>
+  <h1>Does "Header "work"?</h1>
+  <div id="#app">
+    <div :style="{'backgroundImage': '@/assets/Header/WFW_Noordzee_Header.jog'}"></div>
+  </div>
 </template>
+
+
+<!--Achtergrond foto -->
+<!--Logo links | Fixed grootte -->
+<!--Logo Rechts | Fixed grootte-->
+<!--Titel | Text wat groter dan subtitel-->
+<!--Subtitel | Rechterbuitenkant van titel-->
+<!-- Tussenruimte tussen titels-->
+<!--Opslaan in src/assets -->
+
 
 <script>
 export default {
-  name: "Header-item"
+  name: "Header-item", data() {
+    return {
+      image: "@/assets/components/WFW_Noordzee_Header.jpg"
+    }
+  }
 }
+
 </script>
 
 <style scoped>
 
 </style>
+
+<!--  <header-item ref="@/assets/WFW_Noordzee_Header.jpg"></header-item>-->
--- paz-vue/src/main.js
+++ paz-vue/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
+// import Header from "@/components/Header";
+// import Welcome from "@/components/Welcome";
 
 createApp(App).mount('#app')
