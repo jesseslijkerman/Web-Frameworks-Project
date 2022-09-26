f1c138ee18931ce0cf6694634a6b0df3352546a6
Jesse Slijkerman <jesse.slijkerman2@hva.nl>
2022-09-27 00:04:13 +0200

    cabin details form created

--- paz-vue/src/components/cabins/Detail32.vue
+++ paz-vue/src/components/cabins/Detail32.vue
@@ -1,13 +1,63 @@
 <template>
+  <form>
+    <label for="type">Type</label>
+    <select id="type" name="type">
+      <option value="beach-gear" :selected="{'selected' : selectedCabin.type === 'Beach Gear'}">Beach Gear</option>
+      <option value="small-day-time" :selected="{'selected' : selectedCabin.type === 'Small Day Time'}">Small Day Time</option>
+      <option value="small-lodge">Small Lodge</option>
+      <option value="large-lodge">Large Lodge</option>
+      <option value="Family-Lodge">Family Lodge</option>
+    </select>
 
+    <label for="location">Location</label>
+    <input type="text" id="location" name="location" :value="selectedCabin.location">
+
+    <label for="description">Description</label>
+    <input type="text" id="description" name="description" :value="selectedCabin.description">
+
+    <label for="img">Image</label>
+    <input type="text" id="img" name="image" :value="selectedCabin.image">
+
+    <label for="price">Price per week</label>
+    <input type="text" id="price" name="price-per-week" :value="selectedCabin.pricePerWeek">
+
+    <label for="available">Total availability</label>
+    <input type="text" id="available" name="total-availability" :value="selectedCabin.numAvailable">
+  </form>
 </template>
 
 <script>
+import {Cabin} from "@/models/cabin";
+
 export default {
-  name: "Detail32"
+  name: "CabinDetail32",
+  props: {
+    selectedCabin: {
+      type: Cabin,
+      required: true
+    }
+  },
 }
 </script>
 
 <style scoped>
+form{
+  margin: auto;
+  width: 80%;
+}
+
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+label{
+  float: left;
+}
 
 </style>
--- paz-vue/src/components/cabins/Overview32.vue
+++ paz-vue/src/components/cabins/Overview32.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="scroll">
-      <div v-bind:class="['card', { 'selected' : selectedCabin === cabin }]" v-for="cabin in cabins" v-bind:key="cabin.id" @click="selectCabin(cabin)">
+      <div :class="['card', { 'selected' : selectedCabin === cabin }]" v-for="cabin in cabins" v-bind:key="cabin.id" @click="selectCabin(cabin)">
         <img :src="cabin.image">
         <p>{{ cabin.type }}</p>
         <p>{{ cabin.location }}</p>
@@ -9,13 +9,19 @@
     </div>
   </div>
   <button @click="onNewCabin">Add Cabin</button>
+  <p v-if="selectedCabin === null">Select a cabin for details</p>
+  <CabinDetail32 v-else v-bind:selected-cabin="selectedCabin"></CabinDetail32>
 </template>
 
 <script>
 import {Cabin} from "@/models/cabin";
+import CabinDetail32 from "@/components/cabins/Detail32";
 
 export default {
   name: "CabinsOverview32",
+  components: {
+    CabinDetail32
+  },
   created() {
     this.lastId = 10000
     for (let i = 0; i < 8; i++) {
@@ -37,7 +43,9 @@ export default {
       return this.lastId
     },
     onNewCabin(){
-      this.cabins.push(Cabin.createSampleCabin(this.nextId()))
+      let newCabin = Cabin.createSampleCabin(this.nextId())
+      this.cabins.push(newCabin)
+      this.selectedCabin = newCabin
     },
     selectCabin(cabin){
       if (this.selectedCabin === cabin){
@@ -61,14 +69,13 @@ img{
 .container{
   display: flex;
   justify-content: center;
+  margin: auto;
+  width: 80%;
 }
 
 .scroll {
   display: flex;
-  justify-content: center;
   overflow-x: scroll;
-
-  max-width: 800px;
 }
 
 .card{
@@ -77,7 +84,9 @@ img{
 }
 
 .selected{
-  background-color: gray;
+  background-color: palegoldenrod;
+  color: #6e540a;
+  border: 1px solid darkgoldenrod;
 }
 
 </style>
