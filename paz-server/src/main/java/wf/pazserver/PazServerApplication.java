96db8efdd84078d1640d962010ab9735985f41a9
Jesse Slijkerman <jesse.slijkerman2@hva.nl>
2022-10-24 11:20:01 +0200

    Added back end

---
+++ paz-server/src/main/java/wf/pazserver/PazServerApplication.java
@@ -0,0 +1,13 @@
+package wf.pazserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PazServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PazServerApplication.class, args);
+	}
+
+}
