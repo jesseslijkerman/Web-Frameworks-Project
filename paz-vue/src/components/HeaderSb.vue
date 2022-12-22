fa22b78fc443209047b75db459bcf8767b4a1529
Y12Y12 <youssef.khalil@hva.nl>
2022-12-22 11:59:40 +0100

    4.4 changes

---
+++ paz-vue/src/components/HeaderSb.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="header" :style="image">
+    <div class="left-side">
+      <img class="image" id="parasol" src="../assets/parasol.png">
+      <div  class="date">
+        <p>Today is:</p>
+        <p>{{getCurrentDate()}}</p>
+      </div>
+    </div>
+    <div class="title">
+      <h1>{{siteName}}</h1>
+      <h2>Come enjoy the waves!</h2>
+    </div>
+    <img class="image" id="zeester" src="../assets/zeester.png">
+  </div>
+</template>
+
+
+<!--Achtergrond foto -->
+<!--Logo links | Fixed grootte -->
+<!--Logo Rechts | Fixed grootte-->
+<!--Titel | Text wat groter dan subtitel-->
+<!--Subtitel | Rechterbuitenkant van titel-->
+<!-- Tussenruimte tussen titels-->
+<!--Opslaan in src/assets -->
+
+
+<script>
+export default {
+  name: "HeaderSb",
+  inject: ['sessionService'],
+  data() {
+    return {
+      siteName: "Play & Stay aan Zee",
+      image: "../assets/header.jpg",
+      currentDate: ""
+    };
+
+  },
+  methods: {
+    getCurrentDate(){
+      const date = new Date();
+      const options = {
+        dateStyle: "full",
+      }
+      let currentDate = date.toLocaleString("en-GB", options);
+      console.log(currentDate)
+      return currentDate;
+    },
+    test(){
+      this.sessionService.asyncSignIn()
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.header{
+  padding: 10px;
+  background-image: url("../assets/header.jpg");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 200px;
+  margin: 0;
+  object-fit: cover;
+}
+
+.left-side{
+  display: flex;
+  justify-self: left;
+}
+
+.date{
+  align-self: end;
+  text-align: left;
+}
+
+.date p{
+  font-size: large;
+}
+
+h1{
+  align-self: center;
+}
+
+.image{
+  width: auto;
+  height: 150px;
+}
+
+</style>
+
+<!--  <header-item ref="@/assets/WFW_Noordzee_Header.jpg"></header-item>-->
--- paz-vue/src/services/SessionSbService.js
+++ paz-vue/src/services/SessionSbService.js
@@ -1,6 +1,8 @@
 export class SessionSbService {
     BROWSER_STORAGE_ITEM_NAME;
     RESOURCES_URL;
+    currentToken;
+    currentAccount;
 
     constructor(resourceUrl, browserStorageItemName) {
         this.BROWSER_STORAGE_ITEM_NAME = browserStorageItemName;
@@ -12,7 +14,7 @@ export class SessionSbService {
     }
 
     signOut() {
-
+        this.saveTokenIntoBrowserStorage(null, null)
     }
 
     async asyncSignIn(email, password) /* :Promise<User> */ {
@@ -41,8 +43,18 @@ export class SessionSbService {
 
     }
 
-    saveTokenIntoBrowserStorage() {
+    saveTokenIntoBrowserStorage(token, account) {
+        this.currentToken = token;
+        this.currentAccount = account;
 
+        if (token == null) {
+            this.currentAccount == null;
+            window.sessionStorage.removeItem(this.BROWSER_STORAGE_ITEM_NAME);
+            window.sessionStorage.removeItem(this.BROWSER_STORAGE_ITEM_NAME+"ACC");
+        } else {
+            window.sessionStorage.setItem(this.BROWSER_STORAGE_ITEM_NAME, token);
+            window.sessionStorage.setItem(this.BROWSER_STORAGE_ITEM_NAME+"ACC", JSON.stringify(account));
+        }
     }
 
     getTokenFromBrowserStorage() {
