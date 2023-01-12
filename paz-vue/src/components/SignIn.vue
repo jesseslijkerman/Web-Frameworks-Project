25f7eb38e0c2e1006ce8c314ee5d767ea2f3ccc9
Jesse Slijkerman <jesse.slijkerman2@hva.nl>
2023-01-12 21:56:36 +0100

    small fix

--- paz-vue/src/components/SignIn.vue
+++ paz-vue/src/components/SignIn.vue
@@ -1,17 +1,15 @@
 <template>
-<p>Please provide your logon credentials: </p>
+  <p>Please provide your logon credentials:</p>
   <form @submit.prevent="logIn">
 
-  <label for="email">User e-mail:</label>
-  <input v-model="this.email" type="text" name="email">
+    <label for="email">User e-mail:</label>
+    <input v-model="this.email" type="text" id="email">
 
-  <label for="password">Password:</label>
-  <input v-model="this.password" type="text" name="password">
-
+    <label for="password">Password:</label>
+    <input v-model="this.password" type="text" id="password">
     <button>Sign On</button>
   </form>
-
-
+  <p class="errormessage">{{errorMessage}}</p>
 </template>
 
 <script>
@@ -23,19 +21,30 @@ export default {
   data() {
     return{
       email: null,
-      password: null
+      password: null,
+      errorMessage: null
     }
   },
 
   methods: {
     async logIn() {
-      await this.sessionService.asyncSignIn(this.email, this.password)
+      this.errorMessage = null
+      let account = await this.sessionService.asyncSignIn(this.email, this.password)
+      if (account == null){
+        this.errorMessage = "Could not authenticate with provided credentials"
+      } else {
+        this.$router.push("/")
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+
+.errormessage{
+  color: darkred;
+}
 
 form{
   margin: auto;
